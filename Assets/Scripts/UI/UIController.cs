95f105ac3e60ade6281c55802a79d371aca306d2
Sean Jiang <seanjiangsh@gmail.com>
2025-10-23 06:23:31 +0800

    Retrieving the Root Visual Element

--- Assets/Scripts/UI/UIController.cs
+++ Assets/Scripts/UI/UIController.cs
@@ -1,15 +1,21 @@
 using UnityEngine;
+using UnityEngine.UIElements;
 
 namespace RPG.UI
 {
   public class UIController : MonoBehaviour
   {
+    private UIDocument uiDocumentCmp;
+    private VisualElement rootElement;
+
     public UIBaseState currentState;
     public UIMainMenuState mainMenuState;
 
     private void Awake()
     {
       mainMenuState = new UIMainMenuState(this);
+      uiDocumentCmp = GetComponent<UIDocument>();
+      rootElement = uiDocumentCmp.rootVisualElement;
     }
 
     // Start is called before the first frame update
