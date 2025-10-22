75d320bcd786931ff199fa389615057b5a662504
Sean Jiang <seanjiangsh@gmail.com>
2025-10-22 08:02:22 +0800

    Main Menu State

--- Assets/Scripts/UI/UIController.cs
+++ Assets/Scripts/UI/UIController.cs
@@ -3,8 +3,20 @@ using UnityEngine;
 namespace RPG.UI
 {
   public class UIController : MonoBehaviour
-
   {
+    public UIBaseState currentState;
+    public UIMainMenuState mainMenuState;
 
+    private void Awake()
+    {
+      mainMenuState = new UIMainMenuState(this);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      currentState = mainMenuState;
+      currentState.EnterState();
+    }
   }
 }
---
+++ Assets/Scripts/UI/UIMainMenuState.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+namespace RPG.UI
+{
+  public class UIMainMenuState : UIBaseState
+  {
+    public UIMainMenuState(UIController uiController) : base(uiController)
+    {
+
+    }
+
+    public override void EnterState()
+    {
+
+    }
+
+    public override void SelectButton()
+    {
+
+    }
+  }
+}
